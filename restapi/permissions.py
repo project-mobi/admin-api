1e831afd4f658f9ec867277fac46bf11da72e618
Fabian Volkers <fabian.volkers@gmail.com>
2020-03-31 14:42:37 +0200

    fixed permissions so unauthenticated requests receive 403

--- restapi/permissions.py
+++ restapi/permissions.py
@@ -2,7 +2,10 @@ import restapi.models as models
 from rest_framework import permissions
 
 
-class BelongsToOrganisation(permissions.BasePermission):
+class BelongsToOrganisation(permissions.IsAuthenticated):
+    """
+    Extend IsAuthenticated Class with object lever permissions
+    """
     def has_object_permission(self, request, view, obj):
 
         if(type(obj) == type(models.Organisation())):
