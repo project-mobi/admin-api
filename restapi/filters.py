133c72aecceebb0cfad8b3edaac80df36f7083ac
Fabian Volkers <fabian.volkers@gmail.com>
2020-03-31 14:44:44 +0200

    removed check for anonymous user since this is handled in BelongsToOrganisation permission class

--- restapi/filters.py
+++ restapi/filters.py
@@ -1,26 +1,33 @@
 from django_filters import rest_framework as filters
 from restapi import models
 from django.contrib.auth.models import AnonymousUser
+from django.template.response import SimpleTemplateResponse
 
 import logging
 
 logger = logging.getLogger('django.server')
 
 class BaseFilter(filters.FilterSet):
+    """
+    Filter Securing API requests based on organisation, except for superusers
+
+    
+    """
     @property
     def qs(self):
         parent = super().qs
         user = getattr(self.request, 'user', None)
-        if (type(user) != type(AnonymousUser())):
-            if user.is_staff:
-                    return parent
-            else:
-                if(type(parent.model) != type(models.Organisation())):
-                    return parent.filter(organisation=user.organisation)
-                else:
-                    return parent.filter(id=user.organisation)
+        if user.is_superuser:
+                return parent
         else:
-            return parent.filter(id=None)
+            if(type(parent.model) != type(models.Organisation())):
+                return parent.filter(organisation=user.organisation)
+            else:
+                return parent.filter(id=user.organisation)
+
+"""
+Extend BaseFilter class with model / view specific filter fields
+"""
 
 class MachineFilter(BaseFilter):
     class Meta:
